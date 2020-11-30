f15816045d757ff460b7b47f702e6c83ffaab9f0
hslam <791874158@qq.com>
2020-11-30 19:47:22 +0800

    add shmget shmat shmdt

--- ipc_test.go
+++ ipc_test.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"unsafe"
 )
 
 func TestIPC(t *testing.T) {
@@ -65,6 +66,96 @@ func TestIPC(t *testing.T) {
 	shmid, data, _ := Shmgetat(key, 128, 0600)
 	defer Shmrm(shmid)
 	defer Shmdt(data)
+	msgid, _ := Msgget(key, 0600)
+	defer Msgrm(msgid)
+
+	m, err := Msgrcv(msgid, 1, 0600)
+	if err != nil {
+		return
+	}
+	length := int(m[0])
+	if ret, err := Semgetvalue(semid, semnum); err != nil {
+		t.Error(err)
+	} else if ret < 1 {
+		t.Error()
+	}
+	if _, err := Semp(semid, semnum, SEM_UNDO); err != nil {
+		return
+	}
+	text := string(data[:length])
+	if _, err := Semv(semid, semnum, SEM_UNDO); err != nil {
+		return
+	}
+
+	if context != string(text) {
+		t.Error(context, string(text))
+	}
+	<-done
+}
+
+func TestMore(t *testing.T) {
+	context := strings.Repeat("1", 64)
+	done := make(chan struct{})
+	semnum := 0
+	go func() {
+		key, _ := Ftok("/tmp", 0x22)
+		semid, err := Semget(key, 1, 0600)
+		if err != nil {
+			semid, err = Semget(key, 1, IPC_CREAT|IPC_EXCL|0600)
+			if err != nil {
+				panic(err)
+			}
+			_, err := Semsetvalue(semid, semnum, 1)
+			if err != nil {
+				panic(err)
+			}
+		}
+		defer Semrm(semid)
+		shmid, data, _ := Shmgetat(key, 128, IPC_CREAT|0600)
+		defer Shmrm(shmid)
+		defer Shmdt(data)
+		msgid, _ := Msgget(key, IPC_CREAT|0600)
+		defer Msgrm(msgid)
+		if _, err := Semp(semid, semnum, SEM_UNDO); err != nil {
+			return
+		}
+		copy(data, context)
+		if _, err := Semv(semid, semnum, SEM_UNDO); err != nil {
+			return
+		}
+		if err := Msgsnd(msgid, 1, []byte{byte(len(context))}, 0600); err != nil {
+			return
+		}
+
+		time.Sleep(time.Millisecond * 200)
+		close(done)
+	}()
+	time.Sleep(time.Millisecond * 100)
+
+	key, _ := Ftok("/tmp", 0x22)
+	semid, err := Semget(key, 1, 0600)
+	if err != nil {
+		semid, err = Semget(key, 1, IPC_CREAT|IPC_EXCL|0600)
+		if err != nil {
+			panic(err)
+		}
+		_, err := Semsetvalue(semid, semnum, 1)
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer Semrm(semid)
+	size := 128
+	shmid, _ := Shmget(key, size, 0600)
+	defer Shmrm(shmid)
+	shmaddr, _ := Shmattach(shmid, 0600)
+	var sl = struct {
+		addr uintptr
+		len  int
+		cap  int
+	}{shmaddr, size, size}
+	data := *(*[]byte)(unsafe.Pointer(&sl))
+	defer Shmdetach(shmaddr)
 	msgid, _ := Msgget(key, 0600)
 	defer Msgrm(msgid)
 
--- shm.go
+++ shm.go
@@ -7,12 +7,27 @@ import (
 	"github.com/hslam/shm"
 )
 
+// Shmget calls the shmget system call.
+func Shmget(key int, size int, shmFlg int) (int, error) {
+	return shm.Get(key, size, shmFlg)
+}
+
+// Shmattach calls the shmat system call.
+func Shmattach(shmid int, shmFlg int) (uintptr, error) {
+	return shm.Attach(shmid, shmFlg)
+}
+
+// Shmdetach calls the shmdt system call.
+func Shmdetach(addr uintptr) error {
+	return shm.Detach(addr)
+}
+
 // Shmgetat calls the shmget and shmat system call.
 func Shmgetat(key int, size int, shmFlg int) (int, []byte, error) {
 	return shm.GetAt(key, size, shmFlg)
 }
 
-// Shmdt calls the shmdt system call.
+// Shmdt calls the shmdt system call with []byte b.
 func Shmdt(b []byte) error {
 	return shm.Dt(b)
 }
